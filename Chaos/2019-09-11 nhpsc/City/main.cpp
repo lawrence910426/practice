5cf549d0521a34c9e4be8659a2f8061fc303f70b
lawrence910426 <lawrence910426@gmail.com>
2019-12-09 22:47:47 +0800

    update

--- Chaos/2019-09-11 nhpsc/City/main.cpp
+++ Chaos/2019-09-11 nhpsc/City/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <memory.h>
+#include <map>
 
 using namespace std;
-const int MAXN = 1e5 + 50;
+const int MAXN = 1e5 + 50 ,MAXV = 15;
 int leaf;
 struct zkw {
-    int data[MAXN * 3];
-    void modify(int pos ,int val) { for(pos += leaf + 1;pos != 0;pos >>= 1) data[pos] += val; }
-    int query(int l ,int r) {
+    int data[MAXN * 5];
+    void modify(int pos ,long long val) {
+        for(pos += leaf + 1;pos != 0;pos >>= 1) data[pos] += val;
+    }
+    long long query(int l ,int r) {
         int ans = 0;
         for(l += leaf ,r += leaf + 1;l ^ r ^ 1;l >>= 1 ,r >>= 1) {
             if(l % 2 == 0) ans += data[l ^ 1];
@@ -15,40 +18,67 @@ struct zkw {
         }
         return ans;
     }
-} seg[20];
+} seg[MAXV];
 
 int raw[MAXN];
 int main() {
     long long N ,Q ,c ,last_ans ,cmd ,L ,R ,l ,r ,x ,y;
     int i;
     while(cin >> N >> Q >> c) {
-        last_ans = 0;
-        for(leaf = 1;leaf < N + 2;leaf <<= 1) ;
-        for(i = 0;i < 20;i++) memset(seg[i].data ,0 ,sizeof(seg[i].data));
-        for(i = 0;i < N;i++) {
-            cin >> raw[i];
-            seg[raw[i]].modify(i ,1);
-        }
-        while(Q--) {
-            cin >> cmd;
-            if(cmd == 1) {
-                cin >> x >> y;
-                x = (x + c * last_ans) % N;
-                y = (y + c * last_ans) % 100000;
-                seg[raw[x]].modify(x ,-1);
-                seg[y].modify(x ,1);
-                raw[x] = y;
-            } else {
-                cin >> L >> R >> l >> r;
-                L = (L + c * last_ans) % N;
-                R = (R + c * last_ans) % N;
-                l = (l + c * last_ans) % N;
-                r = (r + c * last_ans) % N;
-                R += 1 ,r += 1;
-                for(last_ans = i = 0;i < 20;i++) {
-                    last_ans += seg[i].query(L ,R) * seg[i].query(l ,r);
+        if(N <= 5000 && Q <= 5000 && c == 0) {
+            for(i = 0;i < N;i++) cin >> raw[i];
+            while(Q--) {
+                cin >> cmd;
+                if(cmd == 1) {
+                    cin >> x >> y;
+                    raw[x] = y;
+                } else {
+                    cin >> l >> r >> L >> R;
+                    map<int ,long long> m1 ,m2;
+                    for(i = l;i <= r;i++) m1[raw[i]] = m1[raw[i]] + 1;
+                    for(i = L;i <= R;i++) m2[raw[i]] = m2[raw[i]] + 1;
+                    auto p1 = m1.begin() ,p2 = m2.begin();
+                    last_ans = 0;
+                    while(!(p1 == m1.end() && p2 == m2.end())) {
+                        if(p1 == m1.end()) p2++;
+                        else if(p2 == m2.end()) p1++;
+                        else if(p1->first < p2->first) p1++;
+                        else if(p1->first > p2->first) p2++;
+                        else {
+                            last_ans += p1->second * p2->second;
+                            p1++ ,p2++;
+                        }
+                    }
+                    cout << last_ans << endl;
                 }
-                cout << last_ans << endl;
+            }
+        } else {
+            last_ans = 0;
+            for(leaf = 1;leaf < N + 2;leaf <<= 1) ;
+            for(i = 0;i < MAXV;i++) memset(seg[i].data ,0 ,sizeof(seg[i].data));
+            for(i = 0;i < N;i++) {
+                cin >> raw[i];
+                seg[raw[i]].modify(i ,1);
+            }
+            while(Q--) {
+                cin >> cmd;
+                if(cmd == 1) {
+                    cin >> x >> y;
+                    x = (x + c * last_ans) % N;
+                    y = (y + c * last_ans) % 100000;
+                    seg[raw[x]].modify(x ,-1);
+                    seg[y].modify(x ,1);
+                    raw[x] = y;
+                } else {
+                    cin >> L >> R >> l >> r;
+                    L = (L + c * last_ans) % N;
+                    R = (R + c * last_ans) % N;
+                    l = (l + c * last_ans) % N;
+                    r = (r + c * last_ans) % N;
+                    R += 1 ,r += 1;
+                    for(last_ans = i = 0;i < MAXV;i++) last_ans += seg[i].query(L ,R) * seg[i].query(l ,r);
+                    cout << last_ans << endl;
+                }
             }
         }
     }
--- Chaos/2019-09-11 nhpsc/helicoptor/main.cpp
+++ Chaos/2019-09-11 nhpsc/helicoptor/main.cpp
@@ -3,34 +3,73 @@
 #include <set>
 
 using namespace std;
-const int MAXN = 2e5;
+const int MAXN = 2e5 ,BUCKS = 300;
 int N ,Q ,W ,ans ,weight;
+int buckets[BUCKS] ,_clone[BUCKS];
+int i ,j ,val ,x ,y ,tmp;
+int raw[MAXN];
 int main() {
-    int i ,val;
     while(cin >> N >> Q >> W) {
-        multiset<int> S;
-        for(i = 0;i < N;i++) {
-            cin >> val;
-            S.insert(val);
-        }
-        while(Q--) cin >> i;
-        ans = 0;
-        while(!S.empty()) {
-            weight = W - *S.rbegin();
-            //cout <<*S.rbegin() << " ";
-            S.erase(prev(S.end()));
-            ans += 1;
-            auto ptr = S.lower_bound(weight);
-            if(ptr != S.end() && *ptr == weight) {
-                S.erase(ptr);
-                //cout << *ptr << " ";
-            } else if(ptr != S.begin()) {
-                ptr--;
-                S.erase(ptr);
-                //cout << *ptr << " ";
+        if(Q == 0) {
+            multiset<int> S;
+            for(i = 0;i < N;i++) {
+                cin >> val;
+                S.insert(val);
             }
-            //cout << endl;
+            while(Q--) cin >> i;
+            ans = 0;
+            while(!S.empty()) {
+                weight = W - *S.rbegin();
+                S.erase(prev(S.end()));
+                ans += 1;
+                auto ptr = S.lower_bound(weight);
+                if(ptr != S.end() && *ptr == weight) {
+                    S.erase(ptr);
+                } else if(ptr != S.begin()) {
+                    ptr--;
+                    S.erase(ptr);
+                }
+            }
+            cout << ans << endl;
+        } else {
+            for(i = 0;i < BUCKS;i++) buckets[i] = 0;
+            for(i = 0;i < N;i++) {
+                cin >> raw[i];
+                buckets[raw[i]] += 1;
+            }
+
+            do {
+                ans = 0;
+                for(i = 0;i < BUCKS;i++) _clone[i] = buckets[i];
+                for(i = W;i > 0;i--) {
+                    for(j = W - i;_clone[i] > 0 && j > 0;j--) {
+                        if(i != j) {
+                            if(_clone[j] <= 0) continue;
+                            tmp = min(_clone[j] ,_clone[i]);
+                            ans += tmp;
+                            _clone[j] -= tmp;
+                            _clone[i] -= tmp;
+                        } else {
+                            ans += _clone[i] / 2;
+                            _clone[i] = _clone[i] % 2;
+                        }
+                    }
+                    if(_clone[i] > 0) {
+                        ans += _clone[i];
+                        _clone[i] = 0;
+                    }
+                }
+                cout << ans << endl;
+
+                if(Q != 0) {
+                    cin >> x >> y;
+                    x -= 1;
+                    //cout << x << " " << y << " " << raw[x] << " " << buckets[raw[x]] << " " << buckets[y] << endl;
+                    buckets[raw[x]] -= 1;
+                    buckets[y] += 1;
+                    raw[x] = y;
+                }
+            } while(Q--);
         }
-        cout << ans << endl;
     }
 }
