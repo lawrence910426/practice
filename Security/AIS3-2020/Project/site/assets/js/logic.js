5a2d55fdd15b9b329952417b37e2b66795efe955
lawrence910426 <lawrence910426@gmail.com>
2020-07-31 18:47:18 +0800

    update

--- Security/AIS3-2020/Project/site/assets/js/logic.js
+++ Security/AIS3-2020/Project/site/assets/js/logic.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
     $('.submitted').hide();
     $("#submit").click(function() {
-        setTimeout(function() {
-            $('.submitted').show();
-            $('#Wait').modal('hide');
-        }, 3000)
+        $.ajax({
+            url: "/robot",
+            title: $("#title").val(),
+            content: $("#content").val()
+        }).always(function(resp) {
+            var ans = resp.answer;
+            $("#Answer_Content").css('background-color', ans)
+            $("#Answer_Content_Text").text("This should be the '" + ans + "' party.")
+            setTimeout(function() {
+                $('.submitted').show();
+                $('#Wait').modal('hide');
+            }, 1000)
+        })
     })
-    
+    $(".reply_send").click(function() {
+        $.ajax({
+            url: "/result",
+            status: $(this).attr("code")
+        }).always(function(resp) {
+            
+            setTimeout(function() {
+                $('#Reply').modal('hide');
+            }, 1000)
+        })
+    })
 })
