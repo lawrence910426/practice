28c3d22463de94592aa7945ef7ea27c6d8455f9d
lawrence910426 <lawrence910426@gmail.com>
2020-09-14 23:33:19 +0800

    Still not too washed up

--- Algo/CF/1409 F/main.cpp
+++ Algo/CF/1409 F/main.cpp
@@ -10,55 +10,49 @@ int dp[2][MAXN][MAXN];
 int main() {
     int N, K, ans = -1;
     string s, t;
+    int i, j, k;
     cin >> N >> K >> s >> t;
-    int i, j, k;  
-    memset(dp[1], -1 ,sizeof(dp[1]));
-    for(j = 0;j <= K;j++) dp[1][j][0] = 0;
-    for(k = 0;k < N;k++) dp[1][0][k] = 0;
 
-    for(j = 0;j <= K;j++) {
-        for(k = 0;k < N;k++) cout << dp[1][j][k] << " ";
-        cout << endl;
-    }
+    if(t[0] == t[1]) {
+        for(ans = i = 0;i < N;i++) ans += (s[i] == t[0] ? 1 : 0);
+        ans = min(ans + K, N);
+        ans = ans * (ans - 1) / 2;
+    } else {
+        memset(dp[1], -1 ,sizeof(dp[1]));
+        for(j = 0;j <= K;j++) dp[1][j][0] = 0;
 
-    for(i = 0;i < N;i++) {
-        memset(dp[0], -1, sizeof(dp));
-        cout << s[i] << " " << t[0] << endl;
-	    for(j = 0;j <= K;j++) for(k = 0;k < N;k++) {
-            if(s[i] == t[0]) {
-                if(k != 0) {
-                    cout << j << " " << k << " " << dp[1][j][k - 1] << endl;
-                    dp[0][j][k] = dp[1][j][k - 1];
-                }
-                
-                if(j != 0) {
-                    dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k]);
-                    if(dp[1][j - 1][k] != -1) dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k] + k);
-                }
-            } else if(s[i] == t[1]) {
-                if(dp[1][j][k] != -1) dp[0][j][k] = dp[1][j][k] + k;
-                
-                if(j != 0) {
-                    if(k != 0) dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k - 1]);
-                    dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k]);
-                }
-            } else {
-                dp[0][j][k] = dp[1][j][k];
+        for(i = 0;i < N;i++) {
+            memset(dp[0], -1, sizeof(dp[0]));
+            for(j = 0;j <= K;j++) dp[1][j][0] = 0;
 
-                if(j != 0) {
-                    if(k != 0) dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k - 1]);
-                    dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k] + k);
+            for(j = 0;j <= K;j++) for(k = 0;k < N;k++) {
+                if(s[i] == t[0]) {
+                    if(k != 0) dp[0][j][k] = dp[1][j][k - 1];
+
+                    if(j != 0) {
+                        dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k]);
+                        if(dp[1][j - 1][k] != -1) dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k] + k);
+                    }
+                } else if(s[i] == t[1]) {
+                    if(dp[1][j][k] != -1) dp[0][j][k] = dp[1][j][k] + k;
+
+                    if(j != 0) {
+                        if(k != 0) dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k - 1]);
+                        dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k]);
+                    }
+                } else {
+                    dp[0][j][k] = dp[1][j][k];
+
+                    if(j != 0) {
+                        if(k != 0) dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k - 1]);
+                        if(dp[1][j - 1][k] != -1) dp[0][j][k] = max(dp[0][j][k], dp[1][j - 1][k] + k);
+                    }
                 }
             }
+            swap(dp[0], dp[1]);
         }
-	    swap(dp[0], dp[1]);
-        cout << i << " ---------------- " << endl;
-        for(j = 0;j <= K;j++) {
-            for(k = 0;k < N;k++) cout << dp[1][j][k] << " ";
-            cout << endl;
-        }
+        for(j = 0;j <= K;j++) for(k = 0;k < N;k++) ans = max(dp[1][j][k], ans);
     }
-    
-    for(j = 0;j < K;j++) for(k = 0;k < N;k++) ans = max(dp[1][j][k], ans);
+
     cout << ans << endl;
 }
