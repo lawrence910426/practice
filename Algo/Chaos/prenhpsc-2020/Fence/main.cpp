c5136901ff7ef0b1947b13772c4d579c0de57d8f
root <lawrence910426@gmail.com>
2020-12-06 00:02:50 +0800

    Validating the problem.

---
+++ Algo/Chaos/prenhpsc-2020/Fence/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const int MAXN = 4e5 + 50;
+struct { int x, y; } pt[MAXN];
+
+int main() {
+    int N;
+    cin >> N;
+    int i;
+    for(i = 0;i < N;i++) cin >> pt[i].x >> pt[i].y;
+    
+}
