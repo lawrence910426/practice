890a41b910805de6eec70882a4dfdb261d13e601
lawrence910426 <lawrence910426@gmail.com>
2022-03-11 08:23:24 -0800

    Update lab01.cpp

--- SCC-2021/Day3/lab01.cpp
+++ SCC-2021/Day3/lab01.cpp
@@ -1,39 +1,46 @@
 #include <mpi.h>
-#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <assert.h>
 
 #define ROOT 0
 
-int sqrt(long long x) {
-	long long l = 0, r = x, mid;
-	while(l != r - 1) {
-		mid = (l + r) >> 1;
-		if(mid * mid <= x) l = mid;
-		else r = mid;
-	}
-	return l;
-}
+using namespace std;
 
 int main(int argc, char** argv) {
-	MPI_Init(NULL, NULL);
-	
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+        unsigned long long
+                radius = atoll(argv[1]),
+                prime = atoll(argv[2]),
+                ans = 0, i, j,
+                sqrtRadiusHigh = ceil(radius / sqrt(2)),
+                Top = floor(radius / sqrt(2)) + 1;
 
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	
-	int radius;
-	if(world_rank == ROOT) scanf("[ROOT] Input the desired radius: %d", &radius);
-	MPI_Bcast(&radius, 1, MPI::INT, ROOT, MPI_COMM_WORLD);
+        MPI_Init(NULL, NULL);
+        int world_size, world_rank;
+        MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+        MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-	int ans = 0;
-	int low = radius * world_rank / world_size, up = radius * (world_rank + 1) / world_size;
-	
-	for(int i = low;i < up;i++) 
-		ans += sqrt(radius * radius - i * i);
-	
-	MPI_Barrier(MPI_COMM_WORLD);
-	int sum;
-	MPI_Reduce(&ans, &sum, 1, MPI::INT, MPI_SUM, ROOT, MPI_COMM_WORLD);
-	printf("[%d] The answer is: %d\n", world_rank, sum);
-} 
+        unsigned long long
+                low = Top * world_rank / world_size,
+                up = Top * (world_rank + 1) / world_size,
+                increment;
+
+        j = ceil(sqrt(radius * radius - low * low));
+        for(i = low; i < up; i++) {
+                if((j - 1) * (j - 1) >= (radius + i) * (radius - i))
+                        j -= 1;
+                increment = (j - i - 1) % prime;
+                ans = (ans + increment) % prime;
+        }
+        ans = ans % prime;
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        unsigned long long sum = 0;
+        MPI_Reduce(&ans, &sum, 1, MPI::LONG_LONG_INT, MPI_SUM, ROOT, MPI_COMM_WORLD);
+        sum = sum % prime;
+        sum = (sum + sum + sqrtRadiusHigh) % prime;
+        sum = (sum * 4) % prime;
+
+        if(world_rank == ROOT)
+                cout << sum << endl;
+}
